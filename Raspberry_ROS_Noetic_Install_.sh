3c9edb8c424467bf01d91d2f8cad07d47f948e5e
Yen-Wei-Liang <127264553+Yen-Wei-Liang@users.noreply.github.com>
2023-05-16 16:58:17 +0800

    Update Raspberry_ROS_Noetic_Install_.sh

--- Raspberry_ROS_Noetic_Install_.sh
+++ Raspberry_ROS_Noetic_Install_.sh
@@ -18,23 +18,20 @@ echo "  ≡≡≡　◎　―◎ 　⊃ 　⊃　　　　  2023  　祝大家�
 echo ""
 echo "[Note] Target OS version  >>> Ubuntu 20.04.x (Focal Fossa) or Linux Mint 21.x or Raspbian_buster"
 echo "[Note] Target ROS version >>> ROS Noetic Ninjemys"
-echo "[Note] Catkin workspace   >>> $HOME/catkin_ws"
+echo "[Note] Catkin workspace   >>> $HOME/embedded_catkin_ws"
 echo ""
 echo "PRESS [ENTER] TO CONTINUE THE INSTALLATION"
 echo "IF YOU WANT TO CANCEL, PRESS [CTRL] + [C]"
 read
  
 echo "[Set the target OS, ROS version and name of catkin workspace]"
-name_os_version=${name_os_version:="focal"}
-name_ros_version=${name_ros_version:="noetic"}
-name_catkin_workspace=${name_catkin_workspace:="embedded_catkin_ws"}
 
 
-echo "[Set Swqp]"
+echo "[Set Swap]"
 
 cd /var
 sudo swapoff /var/swap
-sudo dd if=/dev/zero of=swap bs=1M count=2048
+sudo dd if=/dev/zero of=swap bs=1M count=2048 status=progress
 sudo mkswap /var/swap
 sudo swapon /var/swap
 
@@ -49,25 +46,20 @@ echo "[Update the package lists]"
 sudo apt update -y
 sudo apt upgrade -y
 
-
-echo "[Install ros-desktop-full version of Noetic"
-sudo apt install -y ros-$name_ros_version-desktop-full
-
 echo "[Environment setup and getting rosinstall]"
-source /opt/ros/$name_ros_version/setup.sh
+source /opt/ros/noetic/setup.sh
 sudo apt-get install -y python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential cmake
 
 
 echo "[Initialize rosdep and Update]"
-sudo sh -c "rosdep init"
+sudo rosdep init
 rosdep update
 
 echo "[Make the catkin workspace and test the catkin_make]"
-mkdir -p $HOME/$name_catkin_workspace/src
-cd $HOME/$name_catkin_workspace/src
-catkin_init_workspace
-cd $HOME/$name_catkin_workspace
-catkin_make
+sudo mkdir -p /home/pi/embedded_catkin_ws
+cd /home/pi/embedded_catkin_ws
+
+rosinstall_generator ros_comm --rosdistro noetic --deps --wet-only --tar > noetic-ros_comm-wet.rosinstall
 
 
 echo "[Install rosdep and Update]"
@@ -77,7 +69,7 @@ rosdep install -y --from-paths src --ignore-src --rosdistro noetic -r --os=debia
 
 echo "[Set the ROS evironment]"
 
-sudo src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/noetic -j1 -DPYTHON_EXECUTABLE=/usr/bin/python3
+sudo src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/noetic -j4 -DPYTHON_EXECUTABLE=/usr/bin/python3
 
 
 source /opt/ros/noetic/setup.bash
